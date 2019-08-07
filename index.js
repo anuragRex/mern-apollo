1d4864c1fce71438258249920208658792d801db
anuragRex <anurag.rexweb@gmail.com>
2019-08-07 13:34:52 +0530

    2nd commit- express-graphql way

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const app = express();
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const crypto = require('crypto');
 
-// Sample Database
+
 const db = {
     users : [
-        { id : '1', email : 'alex@gmail.com', name : 'Alex' },
-        { id : '2', email : 'max@gmail.com', name : 'Max' }
+        { id : '1', email : 'alex@gmail.com', name : 'Alex', avtarUrl : 'https://navtar.com/alex'},
+        { id : '2', email : 'max@gmail.com', name : 'Max', avtarUrl : 'https://navtar.com/max'}
+    ],
+    messages : [
+        {id : '1', userId : '1', body : 'Heyyy', createdAt : Date.now()},
+        {id : '2', userId : '2', body : 'Hello', createdAt : Date.now()},
+        {id : '3', userId : '1', body : 'what\'s up', createdAt : Date.now()},
     ]
 }
+
+class User {
+    constructor(user){
+        Object.assign(this, user)
+    }
+    
+    messages () {
+        return db.messages.filter(message => message.userId === this.id);
+    }
+}
+
 
 const schema = buildSchema(`
     type Query {
         users : [User!]!
+        user(id : ID!) : User
+        messages : [Message!]!
+    }
+
+    type Mutation {
+        addUser(email : String!, name : String, ) : User
     }
 
     type User {
@@ -18,29 +44,39 @@ const schema = buildSchema(`
         email : String!
         name : String
         avtarUrl : String
+        messages : [Message!]!
+    }
+
+    type Message {
+        id : ID!
+        userId : ID!
+        body : String!
+        createdAt : String
     }
 `);
 
 const rootValue = {
-    //message : () => 'GraphQL works'
-    users : () => db.users
+    users : () => db.users.map(user => new User(user)),
+    messages : () => db.messages,
+    addUser : ({ name, email}) => {
+        const user = {
+            id : crypto.randomBytes(10).toString('hex'),
+            name,
+            email,
+            avtarUrl : 'https://navtar.com/...'
+        }
+
+        db.users.push(user);
+        return user;
+    },
+    user : args => db.users.find(el => el.id === args.id)
 }
 
-graphql(
+
+app.use('/graphql', graphqlHTTP({
     schema,
-    //query,
-    `
-        {
-            users { 
-                email
-            }
-        }
-    
-    `,
-    rootValue
-).then(
-    //console.log
-    res => console.dir(res, {depth : null})
-).catch(
-    console.error
-)
+    rootValue,
+    graphiql : true
+}));
+
+app.listen(4000, () => console.log('listening on 4000'));
