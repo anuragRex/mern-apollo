4214bc458473bb6aa7d5d15f1edc6409f08ca0f6
anuragRex <anurag.rexweb@gmail.com>
2019-08-07 12:05:30 +0530

    1st commit - end of intro

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const { graphql, buildSchema } = require('graphql');
 
+// Sample Database
+const db = {
+    users : [
+        { id : '1', email : 'alex@gmail.com', name : 'Alex' },
+        { id : '2', email : 'max@gmail.com', name : 'Max' }
+    ]
+}
+
 const schema = buildSchema(`
     type Query {
-        message : String
+        users : [User!]!
+    }
+
+    type User {
+        id : ID!
+        email : String!
+        name : String
+        avtarUrl : String
     }
 `);
 
 const rootValue = {
-    message : () => 'GraphQL works'
+    //message : () => 'GraphQL works'
+    users : () => db.users
 }
 
 graphql(
@@ -15,13 +31,16 @@ graphql(
     //query,
     `
         {
-            message
+            users { 
+                email
+            }
         }
     
     `,
     rootValue
 ).then(
-    console.log
+    //console.log
+    res => console.dir(res, {depth : null})
 ).catch(
     console.error
 )
